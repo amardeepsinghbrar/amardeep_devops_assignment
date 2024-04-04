577b2204815310f48e7ec5c40f35632d26a76673
Amardeep Singh <amardeepsinghbrar@gmail.com>
2024-04-04 12:32:27 +0530

    draft

--- Iaac/modules/aws-key-pair/main.tf
+++ Iaac/modules/aws-key-pair/main.tf
@@ -1,5 +1,11 @@
 # Creating key-pair on AWS using SSH-public key
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "aws_ec2_keypair" {
   key_name   = var.aws_ec2_keypair_name
-  public_key = file("${path.module}/my-key.pub")
+  public_key = tls_private_key.example.public_key_openssh
 }
--- Iaac/modules/aws-key-pair/outputs.tf
+++ Iaac/modules/aws-key-pair/outputs.tf
@@ -1,3 +1,10 @@
 output "aws_ec2_keypair" {
     value =  aws_key_pair.aws_ec2_keypair.id
 }
+output "private_key" {
+  value     = tls_private_key.example.private_key_pem
+  sensitive = true
+}
+output "public_key" {
+  value     = tls_private_key.example.public_key_pem
+}
