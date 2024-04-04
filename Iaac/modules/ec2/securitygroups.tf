fa78eb9064f5fffe8dd9eb4da1e9ff7b9bc35521
Amardeep Singh <amardeepsinghbrar@gmail.com>
2024-04-04 13:36:17 +0530

    draft

--- Iaac/modules/ec2/securitygroups.tf
+++ Iaac/modules/ec2/securitygroups.tf
@@ -8,14 +8,14 @@ resource "aws_security_group" "jenkins_ec2_security_group" {
         from_port               = 80
         to_port                 = 80
         protocol                = "tcp"
-        cidr_blocks             = [var.vpc_cidr_block]
+        cidr_blocks             = ["0.0.0.0/0"]
     }
     ingress  {
         description             = "HTTP port 443 from VPC"
         from_port               = 443
         to_port                 = 443
         protocol                = "tcp"
-        cidr_blocks             = [var.vpc_cidr_block]
+        cidr_blocks             = ["0.0.0.0/0"]
     }
     ingress  {
         description             = "SSH"
