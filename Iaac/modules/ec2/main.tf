9554ca75182c7cf177978d3d546dd506ee5ab155
Amardeep Singh <amardeepsinghbrar@gmail.com>
2024-04-04 23:17:07 +0530

    deploy

--- Iaac/modules/ec2/main.tf
+++ Iaac/modules/ec2/main.tf
@@ -27,12 +27,32 @@ resource "aws_instance" "ec2-instance" {
 #!/bin/bash
 
 # Update the package lists
+echo "===Update the package lists==="
 yum update -y
 
 #installing git (Precondition for jenkins docker commands)
+echo "===installing git==="
 yum install git -y
 
+#uninstalling existing old aws cli. Else this will lead to  client.authentication.k8s.io/v1alpha1 error.
+echo "===uninstalling aws==="
+sudo yum remove awscli
+
+#installing aws cli
+echo "===installing aws==="
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Installing kubectl for kubernetes 1.29
+echo "===installing kubectl==="
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+cp ./kubectl /usr/local/bin
+
+
 # Installing Docker
+echo "===installing docker==="
 amazon-linux-extras install docker -y
 systemctl enable docker
 systemctl start docker
@@ -40,22 +60,19 @@ systemctl start docker
 # Add the ec2-user to the docker group
 usermod -a -G docker ec2-user
 
-# Installing kubectl
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-cp ./kubectl /usr/local/bin
-
-#mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && cp ./kubectl /usr/local/bin && export PATH=$HOME/bin:$PATH
 
 # Installing Helm
+echo "===installing Helm==="
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
 # installing java-17-amazon-corretto
+echo "===installing java17==="
 yum  install java-17-amazon-corretto -y
 
 # installing jenkins
+echo "===installing jenkins==="
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install jenkins -y
@@ -63,6 +80,7 @@ systemctl enable jenkins
 systemctl start jenkins
 
 #Adding Jenkins in docker group
+echo "===Adding Jenkins in docker group==="
 usermod -aG docker jenkins
 
 EOF
