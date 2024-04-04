023e96940aa1e2bfabbedc4674d4092365f584f3
Amardeep Singh <amardeepsinghbrar@gmail.com>
2024-04-04 11:49:03 +0530

    Added IAAC draft code

--- Iaac/vpc/provider.tf
+++ Iaac/vpc/provider.tf
@@ -1,16 +1,18 @@
 terraform {
-  backend "s3" {
-      bucket                      = "mys3bucket-terraform"
-      key                         = "${var.environment}/vpc/terraform.tfstate"
-      dynamodb_table              = "terraform-lock"
-      region                      = var.region
-  }
-
   required_providers {
       aws = {
           source  = "hashicorp/aws"
           version = "~> 5.0"
       }
+  }
+}
+
+terraform {
+  backend "s3" {
+      bucket                      = "mys3bucket-terraform"
+      key                         = "${var.environment}/vpc/terraform.tfstate"
+      dynamodb_table              = "terraform-lock"
+      region                      = var.region
   }
   }
 
