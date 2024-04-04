43396e33b4979b422e5713b5e6aa9b9eca22895a
Amardeep Singh <amardeepsinghbrar@gmail.com>
2024-04-04 19:30:01 +0530

    eks draft

--- Iaac/eks/variables.tf
+++ Iaac/eks/variables.tf
@@ -55,31 +55,6 @@ variable "profile" {
     type            = string
     description     = "profile"
 }
-variable "git_platform" {
-    type            = string
-    default         = "GITHUB"
-    description     = "git_platform"
-}
-variable "git_repo_url" {
-    type            = string
-    description     = "git url"
-}
-variable "build_project_name" {
-    type            = string
-    description     = "build_project_name"
-}
-variable "build_project_description" {
-    type            = string
-    description     = "build_project_description"
-}
-variable "dynamodynamodb_table" {
-    type = string
-    description = "dynamodynamodb_table"
-}
-variable "s3_terraform_state_store_bucket" {
-    type = string
-    description = "s3_terraform_state_store_bucket"
-}
 variable "environment" {
     type = string
     description = "environment"
@@ -91,25 +66,4 @@ variable "eks_cluster_name" {
 variable "eks_cluster_version" {
     type = string
     description = "eks_cluster_version"
-}
-#jenkins
-variable "jenkins_ec2_instance_name"{
-    type = string
-    description = "jenkins_ec2_instance_name" 
-}
-variable "jenkins_ec2_security_group_name" {
-    type = string
-    description = "jenkins_ec2_security_group_name" 
-}
-variable "jenkins_ec2_instance_type" {
-    type = string
-    description = "jenkins_ec2_instance_type" 
-}
-variable "jenkins_ec2_associate_public_ip_address" {
-    type = string
-    description = "jenkins_ec2_associate_public_ip_address" 
-}
-variable "jenkins_root_volume" {
-    type = string
-    description = "jenkins_root_volume"
 }
