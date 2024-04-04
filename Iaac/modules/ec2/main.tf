195a351101b544969062b934d590a58738ec1629
Amardeep Singh <amardeepsinghbrar@gmail.com>
2024-04-04 21:42:35 +0530

    eks draft

--- Iaac/modules/ec2/main.tf
+++ Iaac/modules/ec2/main.tf
@@ -42,7 +42,6 @@ usermod -a -G docker ec2-user
 
 # Installing kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
-
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 
